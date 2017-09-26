de047e194f8eb00441d398104e673d32e866d766
Louis Lai <hoangdung2506@gmail.com>
2017-09-27 00:03:53 +0800

    Add Stock Level Transaction

--- app/client.py
+++ app/client.py
@@ -1,10 +1,16 @@
 from cassandra.cluster import Cluster
 from transactions.DummyTransaction import DummyTransaction
+from transactions.StockLevelTransaction import StockLevelTransaction
 
 def run_transactions():
     cluster = Cluster()
     session = cluster.connect('cs4224')
-    dummy_transaction = DummyTransaction(session)
-    dummy_transaction.execute({ 'w_id': 2 })
+    dummy_transaction = StockLevelTransaction(session)
+    dummy_transaction.execute({
+        'w_id': 1,
+        'd_id': 2,
+        't': 400,
+        'l': 3
+    })
 
 run_transactions()
---
+++ app/transactions/StockLevelTransaction.py
@@ -0,0 +1,37 @@
+from Transaction import Transaction
+
+class StockLevelTransaction(Transaction):
+    def execute(self, params):
+        w_id = params['w_id']
+        d_id = params['d_id']
+        threshold = params['t']
+        num_last_orders = params['l']
+
+        next_order_id = self.get_next_order_id(w_id, d_id)
+        last_l_item_ids = self.get_last_l_item_ids(w_id, d_id, next_order_id, num_last_orders)
+        num_items = self.count_items_below_threshold(w_id, last_l_item_ids, threshold)
+        print 'Total Num Items', num_items
+
+
+    def get_next_order_id(self, w_id, d_id):
+        result = self.session\
+            .execute('select d_next_o_id from district where d_w_id = {} and d_id = {}'.format(w_id, d_id))
+        return result[0].d_next_o_id
+
+    def get_last_l_item_ids(self, w_id, d_id, next_order_id, num_last_orders):
+        results = self.session.execute('select ol_i_id from order_line'
+                                  ' where ol_w_id = {} and ol_d_id = {} and ol_o_id >= {}'
+                                  .format(w_id, d_id, next_order_id - num_last_orders))
+        return set(map(lambda result: int(result.ol_i_id), results))
+
+    def count_items_below_threshold(self, w_id, item_ids, threshold):
+        prepared_query = self.session.prepare('select s_quantity from stock where s_w_id = {} and s_i_id = ?'.format(w_id))
+
+        count = 0
+
+        for item_id in item_ids:
+            bound_query = prepared_query.bind([item_id])
+            result = self.session.execute(bound_query)
+            count += 1 if int(result[0].s_quantity) < threshold else 0
+
+        return count
