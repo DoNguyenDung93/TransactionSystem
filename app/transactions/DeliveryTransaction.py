cb0e5e3d4579a08ee9180fb24b34a0a733daa6bb
Do Nguyen Dung <donguyendung.20.09.1993@gmail.com>
2017-09-28 01:08:20 +0800

    Added comment to Delivery Transaction

--- app/transactions/DeliveryTransaction.py
+++ app/transactions/DeliveryTransaction.py
@@ -1,6 +1,10 @@
 import Transaction.Transaction
 import datetime
 
+# Delivery Transaction (Transaction #3)
+# This transaction is used to process the delivery of the oldest yet-to-be-delivered order for each of the 10
+# districts in a specified warehouse.
+
 class DeliveryTransaction(Transaction):
     def execute(self, params):
         w_id = params['w_id']
@@ -15,6 +19,8 @@ class DeliveryTransaction(Transaction):
             self.update_customer_balance(customer_id, sum_order)
             self.update_customer_delivery_cnt(customer_id)
 
+    # Find the order with the smallest O_ID using W_ID and DISTRICT_NO from 1 to 10
+    # and O_CARRIER_ID = null
     def get_smallest_order_number(self, w_id, num):
         result = self.session.execute('select o_id from order_ where o_w_id = {}'
                                       ' and o_d_id = {} and o_carrier_id = null'.format(w_id, num))
@@ -24,18 +30,22 @@ class DeliveryTransaction(Transaction):
                 smallest_result = result[index].o_id
         return smallest_result
 
+    # Find customer corresponding to the smallest order number
     def find_customer(self, smallest_order_number):
         result = self.session.execute('select o_c_id from order_ where o_id = {}'.format(smallest_order_number))
         return result[0].o_c_id
 
+    # Update the O_CARRIER_ID with CARRIER_ID input
     def update_order(self, smallest_order_number, carrier_id):
         self.session.execute('update order_ set o_carrier_id = {} where o_id = {}'
                              .format(carrier_id, smallest_order_number))
 
+    # Update all the order-line in the order by setting OL_DELIVERY_D to the current date and time
     def update_order_line(self, smallest_order_number):
         self.session.execute('update order_line set ol_delivery_d = {} where ol_o_id = {}'
                              .format(str(datetime.datetime.now()), smallest_order_number))
 
+    # Get the sum of value of all the items in the order
     def get_order_sum(self, smallest_order_number):
         result = self.session.execute('select ol_amount from order_line where o_id = {}'.format(smallest_order_number))
         sum_order = 0.0
@@ -43,14 +53,12 @@ class DeliveryTransaction(Transaction):
             sum_order += result[index].ol_amount
         return sum_order
 
-    def get_balance(self, customer_id):
-        result = self.session.execute('select c_balance from customer where c_id = {}'.format(customer_id))
-        return result[0].c_balance
-
+    # Increase the current customer balance with the value of the order
     def update_customer_balance(self, customer_id, sum_order):
         self.session.execute('update customer set c_balance = c_balance + {} where c_id = {}'
                              .format(sum_order, customer_id))
 
+    # Increase the number of delivery to the customer by 1
     def update_customer_delivery_cnt(self, customer_id):
         self.session.execute('update customer set c_delivery_cnt = c_delivery_cnt + 1 where c_id = {}'
                              .format(customer_id))
