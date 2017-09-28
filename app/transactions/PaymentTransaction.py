18f5462458ddadad50bdf73b6ffedf77ff646729
HUNG <nguyenthaihung07@gmail.com>
2017-09-28 16:45:12 +0800

    update PaymentTransaction

--- app/transactions/PaymentTransaction.py
+++ app/transactions/PaymentTransaction.py
@@ -11,7 +11,7 @@ class PaymentTransaction(Transaction):
 		self.update_warehouse(c_w_id, payment)
 		self.update_district(c_w_id, c_d_id, payment)
 		self.update_customer(c_w_id, c_d_id, c_id, payment)
-		self.print_output(c_w_id, c_d_id, c_id)
+		self.print_output(c_w_id, c_d_id, c_id, payment)
 
 	def update_warehouse(self, c_w_id, payment):
 		self.session.execute('update warehouse set w_ytd = w_ytd + {} where w_id = {}'.format(payment, c_w_id))
@@ -22,6 +22,20 @@ class PaymentTransaction(Transaction):
 	def update_customer(self, c_w_id, c_d_id, c_id, payment):
 		self.session.execute('update customer set c_balance = c_balance - {}, c_ytd_payment = c_ytd_payment + {}, c_payment_cnt = c_payment_cnt + 1 where c_id = {} and c_w_id = {} and c_d_id = {}'.format(payment, payment, c_id, c_w_id, c_d_id))
 
-	def print_output(self, c_w_id, c_d_id, c_id):
+	def print_output(self, c_w_id, c_d_id, c_id, payment):
 		customer_info = self.get_customer_info(c_w_id, c_d_id, c_id)
-		print "Customer: {} {} {}".format(customer_info.c_first, customer_info.c_middle, customer_info.c_last)
+		print "Customer:		{} {} {}".format(customer_info.c_first, customer_info.c_middle, customer_info.c_last)
+		print "Address:		{} {} {} {} {}".format(customer_info.c_street_1, customer_info.c_street2, customer_info.c_city, customer_info.c_state, customer_info.c_zip)
+		print "Phone:		{}".format(customer_info.c_phone)
+		print "Since:		{}".format(customer_info.c_since)
+		print "Credit:		{} with limit {}".format(customer_info.c_credit, customer_info.c_credit_lim)
+		print "Discount:		{}".format(customer_info.c_discount)
+		print "Balance:		{}".format(customer_info.c_balance)
+
+		warehouse = self.session.execute('select w_street_1, w_street_2, w_city, w_state, w_zip from warehouse where w_id = {}'.format(c_w_id))
+		print "Warehouse:		{} {} {} {} {}".format(warehouse.w_street_1, warehouse.w_street_2, warehouse.w_city, warehouse.w_state, warehouse.w_zip)
+
+		district = self.session.execute('select d_street_1, d_street_2, d_city, d_state, d_zip from district where d_id = {} and d_w_id = {}'.format(c_d_id, c_w_id))
+		print "District:		{} {} {} {} {}".format(district.d_street_1, district.d_street_2, district.d_city, district.d_state, district.d_zip)
+
+		print "Payment:		{}".format(payment)
