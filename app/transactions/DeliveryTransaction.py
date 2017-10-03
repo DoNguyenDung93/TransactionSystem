b18eb79add92c120bdc439216bcd370f2c0ccb30
Do Nguyen Dung <donguyendung.20.09.1993@gmail.com>
2017-10-04 01:48:25 +0800

    Fixed all bugs in DeliveryTransaction

--- app/transactions/DeliveryTransaction.py
+++ app/transactions/DeliveryTransaction.py
@@ -1,5 +1,6 @@
 from Transaction import Transaction
 from datetime import datetime
+import decimal
 
 # Delivery Transaction (Transaction #3)
 # This transaction is used to process the delivery of the oldest yet-to-be-delivered order for each of the 10
@@ -43,13 +44,19 @@ class DeliveryTransaction(Transaction):
                                                                        'and c_w_id = {} and c_d_id = ?'.format(w_id))
 
         for num in range(1, 10):
-            smallest_order_number = self.get_smallest_order_number(num)
-            customer_id = self.find_customer(smallest_order_number, num)
+            smallest_order_number = int(self.get_smallest_order_number(num))
+            print 'smallest_order_number', smallest_order_number
+            customer_id = int(self.find_customer(smallest_order_number, num))
+            # print 'customer_id', customer_id
             self.update_order(smallest_order_number, carrier_id, num)
-            ol_number = self.get_order_line_number(smallest_order_number, num)
-            for index in range(len(list(ol_number))):
-                self.update_order_line(smallest_order_number, num, ol_number[index])
+            # ol_number = self.get_order_line_number(smallest_order_number, num)
+            # print 'ol_number', ol_number[0].ol_number
+            # for index in range(len(list(ol_number))):
+            for ol_number in self.get_order_line_number(smallest_order_number, num):
+                # print 'ol_number', ol_number
+                self.update_order_line(smallest_order_number, num, ol_number[0])
             sum_order = self.get_order_sum(smallest_order_number, num)
+            # print 'sum_order', sum_order
             current_balance = self.get_customer_balance(customer_id, num)
             self.update_customer_balance(customer_id, sum_order, num, current_balance)
             delivery_cnt = self.get_delivery_count(customer_id, num)
@@ -58,54 +65,56 @@ class DeliveryTransaction(Transaction):
     # Find the order with the smallest O_ID using W_ID and DISTRICT_NO from 1 to 10
     # and O_CARRIER_ID = -1
     def get_smallest_order_number(self, num):
-        result = self.session.execute(self.get_smallest_order_number_query.bind(num))
+        result = self.session.execute(self.get_smallest_order_number_query.bind([num]))
         # smallest_result = result[0].o_id
         # for index in range(len(list(result))):
         #     if result[index].o_id < smallest_result:
         #         smallest_result = result[index].o_id
-        return result
+        return result[0].o_id
 
     # Find customer corresponding to the smallest order number
     def find_customer(self, smallest_order_number, num):
-        result = self.session.execute(self.find_customer_query.bind(smallest_order_number, num))
+        result = self.session.execute(self.find_customer_query.bind([smallest_order_number, num]))
         return result[0].o_c_id
 
     # Update the O_CARRIER_ID with CARRIER_ID input
     def update_order(self, smallest_order_number, carrier_id, num):
-        self.session.execute(self.update_order_query.bind(carrier_id, smallest_order_number, num))
+        self.session.execute(self.update_order_query.bind([carrier_id, smallest_order_number, num]))
 
     # Find all the order-line numbers of each order
     def get_order_line_number(self, smallest_order_number, num):
-        return self.session.execute(self.get_order_line_number_query.bind(smallest_order_number, num))
+        return self.session.execute(self.get_order_line_number_query.bind([smallest_order_number, num]))
 
     # Update all the order-line in the order by setting OL_DELIVERY_D to the current date and time
     def update_order_line(self, smallest_order_number, num, ol_number):
-        time = datetime.utcnow()
+        # time = datetime.utcnow().isoformat(' ')
         self.session.execute(self.update_order_line_query.bind(
-            time.strftime('%Y-%m-%d %H:%M:%S'), smallest_order_number, num, ol_number))
+            [datetime.strptime(datetime.utcnow().isoformat(' '), '%Y-%m-%d %H:%M:%S.%f'), smallest_order_number, num, ol_number]))
 
     # Get the sum of value of all the items in the order
     def get_order_sum(self, smallest_order_number, num):
-        result = self.session.execute(self.get_order_sum_query.bind(smallest_order_number, num))
-        sum_order = 0.0
-        for index in range(len(list(result))):
-            sum_order += result[index].ol_amount
+        result = self.session.execute(self.get_order_sum_query.bind([smallest_order_number, num]))
+        # print 'sum_order', result[0].ol_amount
+        sum_order = decimal.Decimal(0)
+        for index in result:
+            # print 'order_sum', index[0]
+            sum_order = sum_order + index[0]
         return sum_order
 
     # Get the current balance of the customer
     def get_customer_balance(self, customer_id, num):
-        result = self.session.execute(self.get_customer_balance_query.bind(customer_id, num))
+        result = self.session.execute(self.get_customer_balance_query.bind([customer_id, num]))
         return result[0].c_balance
 
     # Increase the current customer balance with the value of the order
     def update_customer_balance(self, customer_id, sum_order, num, current_balance):
-        self.session.execute(self.update_customer_balance_query.bind(current_balance + sum_order, customer_id, num))
+        self.session.execute(self.update_customer_balance_query.bind([current_balance + sum_order, customer_id, num]))
 
     # Get the delivery count of the customer
     def get_delivery_count(self, customer_id, num):
-        result = self.session.execute(self.get_delivery_count_query.bind(customer_id, num))
+        result = self.session.execute(self.get_delivery_count_query.bind([customer_id, num]))
         return result[0].c_delivery_cnt
 
     # Increase the number of delivery to the customer by 1
     def update_customer_delivery_cnt(self, customer_id, num, delivery_cnt):
-        self.session.execute(self.update_customer_delivery_cnt_query.bind(delivery_cnt + 1, customer_id, num))
+        self.session.execute(self.update_customer_delivery_cnt_query.bind([delivery_cnt + 1, customer_id, num]))
