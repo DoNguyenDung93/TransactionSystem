0530f8ac5f8278a8fa9e7d6d699d5aa645bb91cc
Louis Lai <hoangdung2506@gmail.com>
2017-10-05 12:51:34 +0800

    add benchmark script

---
+++ benchmark.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Experiment scripts to be run in sunfire node
+
+# run experiment with NC argument supplied, e.g `./benchmark.sh 20`
+NC=$1
+
+for i in `seq 1 ${NC}`; do
+    serverIdx=$(( 35 + $i % 5 ))
+    log="log${i}.txt"
+    rm -f $log
+    touch $log
+    ssh "cs4224h@xcnd${serverIdx}.comp.nus.edu.sg" \
+     "cd TransactionSystem-master && python app/Client.py < 4224-project-files/xact-files/${i}.txt" \
+     2>&1 | tee -a $log &
+done
