c962255c2cff6c340eb6297d461cea8884dbd29d
HUNG <nguyenthaihung07@gmail.com>
2017-10-07 17:03:04 +0800

    Revise PaymentTransaction

--- app/transactions/NewOrderTransaction.py
+++ app/transactions/NewOrderTransaction.py
@@ -16,27 +16,55 @@ class NewOrderTransaction(Transaction):
 		orders = map(lambda ol: map(int, ol), params['items'])
 
 		# intermediate data
-		w_tax = self.session.execute('SELECT w_tax FROM warehouse WHERE w_id = {}'.format(w_id))
-		w_tax = w_tax[0].w_tax
-		customer = self.session.execute('SELECT c_first, c_middle, c_last, c_credit, c_credit_lim, c_discount FROM customer WHERE c_w_id = {} AND c_d_id = {} AND c_id = {}'.format(w_id, d_id, c_id))
-		customer = customer[0]
-		c_discount = customer.c_discount
+		w_tax = self.get_w_tax(w_id)
+		customer = self.get_customer_for_output(w_id, d_id, c_id)
 
-		# processing steps
+		# processing steps (follow sequence in project.pdf)
 		next_o_id, d_tax = self.get_d_next_o_id_and_d_tax(w_id, d_id)
-		self.update_d_next_o_id(w_id, d_id, next_o_id)
+		self.update_d_next_o_id(w_id, d_id, next_o_id+1)
 		entry_date = self.create_new_order(w_id, d_id, c_id, next_o_id, num_items, orders)
 		print_item_results, total_amount = self.update_stock_and_create_order_line(w_id, d_id, c_id, next_o_id, orders, d_tax, w_tax, c_discount)
-		self.print_output(customer, w_tax, d_tax, next_o_id, entry_date, num_items, total_amount)
+		self.print_output(w_id, d_id, c_id, customer, w_tax, d_tax, next_o_id, entry_date, num_items, total_amount)
 		self.print_items(print_item_results)
 
-	def get_d_next_o_id_and_d_tax(self, w_id, d_id):
-		result = self.session.execute('SELECT d_next_o_id, d_tax FROM district WHERE d_w_id = {} AND d_id = {}'.format(w_id, d_id))
-		return int(result[0].d_next_o_id), int(result[0].d_tax)
+	def get_w_tax(self, w_id):
+		"""Get warehouse tax from the vertical partition table warehouse_tax"""
+		prepared_query = self.session.prepre('SELECT w_tax FROM warehouse_tax WHERE w_id = ?')
+		bound_query = prepared_query.bind([w_id])
+		rows = list(self.session.execute(bound_query))
+		if len(rows) == 0:
+			print "Cannot find any warehouse with w_id {} {} {}".format(w_id)
+			return 0
+		else:
+			return Decimal(rows[0].w_tax)
 
-	def update_d_next_o_id(self, w_id, d_id, next_o_id):
-		new_d_next_o_id = next_o_id + 1
-		self.session.execute('UPDATE district SET d_next_o_id = {} WHERE d_id = {} AND d_w_id = {}'.format(new_d_next_o_id, d_id, w_id))
+	def get_customer_for_output(self, w_id, d_id, c_id):
+		"""Get customer info (c_last, c_credit, c_discount) for printing output"""
+		prepared_query = self.session.prepare('SELECT c_last, c_credit, c_discount FROM customer WHERE  c_w_id = ? AND c_d_id = ? AND c_id = ?')
+		bound_query = prepared_query.bind([w_id, d_id, c_id])
+		rows = list(self.session.execute(bound_query))
+		if len(rows) == 0:
+			print "Cannot find any customer with w_id d_id c_id {} {} {}".format(w_id, d_id, c_id)
+			return None
+		else:
+			return rows[0]
+
+	def get_d_next_o_id_and_d_tax(self, w_id, d_id):
+		"""Get d_next_o_id and d_tax from vertical partition district_next_order_id table"""
+		prepared_query = self.session.prepare('SELECT d_next_o_id, d_tax FROM district_next_order_id WHERE d_w_id = ? AND d_id = ?')
+		bound_query = prepared_query.bind([w_id, d_id])
+		rows = list(self.session.execute(bound_query))
+		if len(rows) == 0:
+			print "Cannot find any district with w_id d_id {} {}".format(w_id, d_id)
+			return 0, 0
+		else:
+			return int(rows[0].d_next_o_id), Decimal(rows[0].d_tax)
+
+	def update_d_next_o_id(self, w_id, d_id, new_d_next_o_id):
+		"""Increment d_next_o_id of district table"""
+		prepared_query = self.session.prepare('UPDATE district SET d_next_o_id = ? WHERE d_id = ? AND d_w_id = ?')
+		bound_query = prepared_query.bind([w_id, d_id, new_d_next_o_id])
+		self.session.execute(bound_query)
 
 	def get_all_local(self, w_id, orders):
 		for (_, supply_warehouse_id, _) in orders:
@@ -45,14 +73,16 @@ class NewOrderTransaction(Transaction):
 		return 1
 
 	def create_new_order(self, w_id, d_id, c_id, o_id, num_items, orders):
+		"""Create a new order (insert row into order_ table)"""
 		all_local = self.get_all_local(w_id, orders)
 		time = datetime.now()
 		prepared_query = self.session.prepare('INSERT INTO order_ (o_w_id, o_d_id, o_id, o_c_id, o_carrier_id, o_ol_cnt, o_all_local, o_entry_d) VALUES (?, ?, ?, ?, ?, ?, ?, ?)')
-		bound_query = prepared_query.bind([int(w_id), int(d_id), int(o_id), int(c_id), None, int(num_items), all_local, time])
+		bound_query = prepared_query.bind([w_id, d_id, o_id, c_id, -1, num_items, all_local, time])
 		self.session.execute(bound_query)
 		return time
 
 	def update_stock_and_create_order_line(self, w_id, d_id, c_id, n, orders, d_tax, w_tax, c_discount):
+		"""Update stock table, create a new order-line for each new item"""
 		total_amount = 0
 		result = []
 		for index, (item_number, supplier_warehouse, quantity) in enumerate(orders):
@@ -90,10 +120,12 @@ class NewOrderTransaction(Transaction):
 		total_amount = total_amount * (1 + d_tax + w_tax) * (1 - c_discount)
 		return result, total_amount
 
-	def print_output(self, customer, w_tax, d_tax, o_id, entry_date, num_items, total_amount):
+	def print_output(self, w_id, d_id, c_id, customer, w_tax, d_tax, o_id, entry_date, num_items, total_amount):
+		"""Print output for the customer"""
 		print
-		print "Customer:		{} {} {}".format(customer.c_first, customer.c_middle, customer.c_last)
-		print "Credit:			{} with limit {}".format(customer.c_credit, customer.c_credit_lim)
+		print "Customer w_d_c_id:	{} {} {}".format(w_id, d_id, c_id)
+		print "Customer lastname:	".format(customer.c_last)
+		print "Credit:			{}".format(customer.c_credit)
 		print "Discount:		{}".format(customer.c_discount)
 		print "Warehouse tax:		{}".format(w_tax)
 		print "District tax:		{}".format(d_tax)
@@ -103,6 +135,7 @@ class NewOrderTransaction(Transaction):
 		print "Total amount:		{}".format(total_amount)
 
 	def print_items(self, items):
+		"""Print output for each item"""
 		for (item_num, i_name, supplier_warehouse, quantity, ol_amount, s_quantity) in items:
 			print
 			print "Item number:		{}".format(item_num)
--- app/transactions/PaymentTransaction.py
+++ app/transactions/PaymentTransaction.py
@@ -13,35 +13,64 @@ class PaymentTransaction(Transaction):
 		# processing steps
 		warehouse_print = self.update_warehouse(c_w_id, payment)
 		district_print = self.update_district(c_w_id, c_d_id, payment)
-		customer_print, c_balance = self.update_customer(c_w_id, c_d_id, c_id, payment)
-		self.print_output(warehouse_print, district_print, customer_print, c_balance, payment)
+		customer_print, new_c_balance = self.update_customer(c_w_id, c_d_id, c_id, payment)
+		self.print_output(c_id, c_w_id, c_d_id, warehouse_print, district_print, customer_print, new_c_balance, payment)
 
-	def update_warehouse(self, c_w_id, payment):
-		row = self.session.execute('SELECT * FROM warehouse WHERE w_id = {}'.format(c_w_id))
-		row = row[0]
-		new_w_ytd = row.w_ytd + Decimal(payment)
-		self.session.execute('UPDATE warehouse SET w_ytd = {} WHERE w_id = {}'.format(new_w_ytd, c_w_id))
-		return row
+	def update_warehouse(self, warehouse_id, payment):
+		"""Increment w_ytd by payment, return warehouse print outputs"""
+		prepared_query = self.session.prepare('SELECT w_street_1, w_street_2, w_city, w_state, w_zip, w_ytd FROM warehouse WHERE w_id = ?')
+		bound_query = prepared_query.bind([warehouse_id])
+		rows = list(self.session.execute(bound_query))
+		if not rows:
+			print "Cannot find any warehouse with w_id {}".format(warehouse_id)
+			return
+		else:
+			row = rows[0]
+			new_w_ytd = row.w_ytd + Decimal(payment)
+			prepared_query = self.session.prepare('UPDATE warehouse SET w_ytd = ? WHERE w_id = ?')
+			bound_query = prepared_query.bind([new_w_ytd, warehouse_id])
+			self.session.execute(bound_query)
+			return row
 
-	def update_district(self, c_w_id, c_d_id, payment):
-		row = self.session.execute('SELECT * FROM district WHERE d_w_id = {} AND d_id = {}'.format(c_w_id, c_d_id))
-		row = row[0]
-		new_d_ytd = row.d_ytd + Decimal(payment)
-		self.session.execute('UPDATE district SET d_ytd = {} WHERE d_w_id = {} AND d_id = {}'.format(new_d_ytd, c_w_id, c_d_id))
-		return row
+	def update_district(self, warehouse_id, district_id, payment):
+		"""Increment d_ytd by payment, return district print outputs"""
+		prepared_query = self.session.prepare('SELECT d_street_1, d_street_2, d_city, d_state, d_zip, d_ytd FROM district WHERE d_w_id = ? AND d_id = ?')
+		bound_query = prepared_query.bind([warehouse_id, district_id])
+		rows = list(self.session.execute(bound_query))
+		if not rows:
+			print "Cannot find any district with w_id d_id {} {}".format(warehouse_id, district_id)
+			return
+		else:
+			row = rows[0]
+			new_d_ytd = row.d_ytd + Decimal(payment)
+			prepared_query = self.session.prepare('UPDATE district SET d_ytd = ? WHERE d_w_id = ? AND d_id = ?')
+			bound_query = prepared_query.bind([new_d_ytd, warehouse_id, district_id])
+			self.session.execute(bound_query)
+			return row
 
-	def update_customer(self, c_w_id, c_d_id, c_id, payment):
-		row = self.session.execute('SELECT * FROM customer WHERE c_id = {} AND c_w_id = {} AND c_d_id = {}'.format(c_id, c_w_id, c_d_id))
-		row = row[0]
-		new_c_balance = row.c_balance - Decimal(payment)
-		new_c_ytd_payment = row.c_ytd_payment + float(payment)
-		new_c_payment_cnt = row.c_payment_cnt + 1
-		self.session.execute('UPDATE customer SET c_balance = {}, c_ytd_payment = {}, c_payment_cnt = {} WHERE c_id = {} AND c_w_id = {} AND c_d_id = {}'.format(new_c_balance, new_c_ytd_payment, new_c_payment_cnt, c_id, c_w_id, c_d_id))
-		return row, new_c_balance
+	def update_customer(self, warehouse_id, district_id, customer_id, payment):
+		"""Decrease c_balance by payment, increase c_ytd_payment by payment, increment c_payment_cnt, return customer print outputs"""
+		prepared_query = self.session.prepare('SELECT c_first, c_middle, c_last, c_street_1, c_street_2, c_city, c_state, c_zip, c_phone, c_since, c_credit, c_discount, c_credit_lim, c_balance, c_ytd_payment, c_payment_cnt FROM customer WHERE c_id = ? AND c_w_id = ? AND c_d_id = ?')
+		bound_query = prepared_query.bind([customer_id, warehouse_id, district_id])
+		rows = list(self.session.execute(bound_query))
+		if not rows:
+			print "Cannot find any customer with c_id w_id d_id {} {} {}".format(customer_id, warehouse_id, district_id)
+			return
+		else:
+			row = rows[0]
+			new_c_balance = row.c_balance - Decimal(payment)
+			new_c_ytd_payment = row.c_ytd_payment + float(payment)
+			new_c_payment_cnt = row.c_payment_cnt + 1
+			prepared_query = self.session.prepare('UPDATE customer SET c_balance = ?, c_ytd_payment = ?, c_payment_cnt = ? WHERE c_id = ? AND c_w_id = ? AND c_d_id = ?')
+			bound_query = prepared_query.bind([new_c_balance, new_c_ytd_payment, new_c_payment_cnt, customer_id, warehouse_id, district_id])
+			self.session.execute(bound_query)
+			return row, new_c_balance
 
-	def print_output(self, warehouse, district, customer, c_balance, payment):
+	def print_output(self, customer_id, warehouse_id, district_id, warehouse, district, customer, c_balance, payment):
+		"""Print required outputs"""
 		print
-		print "Customer:		{} {} {}".format(customer.c_first, customer.c_middle, customer.c_last)
+		print "Customer w_d_c_id:	{} {} {}".format(warehouse_id, district_id, customer_id)
+		print "Customer name:		{} {} {}".format(customer.c_first, customer.c_middle, customer.c_last)
 		print "Address:		{} {} {} {} {}".format(customer.c_street_1, customer.c_street_2, customer.c_city, customer.c_state, customer.c_zip)
 		print "Phone:			{}".format(customer.c_phone)
 		print "Since:			{}".format(customer.c_since)
