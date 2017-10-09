4ce4144c89947318038083ee098c0b9b03542d70
Louis Lai <hoangdung2506@gmail.com>
2017-10-09 21:41:48 +0800

    update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -3,6 +3,7 @@
 
 # run experiment with NC argument supplied, e.g `./benchmark.sh 20`
 NC=$1
+LEVEL=1
 
 for i in `seq 1 ${NC}`; do
     serverIdx=$(( 35 + $i % 5 ))
@@ -10,6 +11,6 @@ for i in `seq 1 ${NC}`; do
     rm -f $log
     touch $log
     ssh "cs4224h@xcnd${serverIdx}.comp.nus.edu.sg" \
-     "cd TransactionSystem-master && python app/Client.py < 4224-project-files/xact-files/${i}.txt" \
+     "cd TransactionSystem-master && python app/Client.py ${LEVEL} < 4224-project-files/xact-files/${i}.txt" \
      2>&1 | tee -a $log &
 done
