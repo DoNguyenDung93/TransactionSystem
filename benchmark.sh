46d5d06c222598e529481428cbe41fa6f8332a0f
Louis Lai <hoangdung2506@gmail.com>
2017-10-14 07:17:13 +0800

    update benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Experiment scripts to be run in sunfire node
 
+echo `date` >> duration.txt
 # run experiment with NC argument supplied, e.g `./benchmark.sh 20`
 NC=$1
 LEVEL=$2
@@ -17,7 +18,28 @@ done
 
 # Wait for all processes to finish and output final db states
 wait
-rm db_state.txt
+rm -f db_state.txt
 touch db_state.txt
 ssh "cs4224h@xcnd35.comp.nus.edu.sg" \
  "cd TransactionSystem-master && python app/FinalOutputer.py ${LEVEL}" 2>&1 | tee -a db_state.txt
+
+echo `date` >> duration.txt
+
+# Get aggregate stats
+
+NR=`ls | grep log | wc -l`
+sum=0.0
+max=-1
+min=10000
+
+for f in `ls | grep log`; do
+  next=`tail -1 $f | awk '{ print $3 }'`
+  sum=`echo $sum $next | awk '{print $1 + $2}'`
+  min=`echo $min $next | awk '{if($2<$1){print $2}else{print $1}}'`
+  max=`echo $min $next | awk '{if($2>$1){print $2}else{print $1}}'`
+done
+
+avg=`echo $sum $NR | awk '{print $1 / $2}'`
+echo "Average throughput: $avg (xacts/s)" | tee -a aggregate.txt
+echo "Min throughput: $min (xacts/s)" | tee -a aggregate.txt
+echo "Max throughput: $max (xacts/s)" | tee -a aggregate.txt
